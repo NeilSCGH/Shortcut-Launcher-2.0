956f5469bf97cf8b3ff0028745be539c93edc6b1
NeilSCGH <neil.segard@gmail.com>
2020-09-14 15:39:27 +0200

    Read settings.csv file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Shortcut_Launcher_2._0
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            IEnumerable<Button> btnList = ReadCSV();
+
+            foreach (Button b in btnList)
+            {
+                Console.WriteLine(b.Name + " " + b.Link);
+            }
         }
+
+        public IEnumerable<Button> ReadCSV()
+        {
+            // TODO: Error checking
+            string[] lines = File.ReadAllLines("settings.csv");
+
+            // lines.Select allows me to project each line as a Button. 
+            // This will give me an IEnumerable<Person> back.
+            return lines.Select(line =>
+            {
+                string[] data = line.Split(',');
+                return new Button(data[0], data[1]);
+            });
+        }
+    }
+
+    public class Button
+    {
+        public Button(string N, string L)
+        {
+            this.Name = N;
+            this.Link = L;
+        }
+        
+        public string Name;
+        public string Link;
     }
 }
